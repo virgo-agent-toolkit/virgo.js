38c7607077c31fed4aa1832478b2051188612b40
Ryan Phillips <ryan.phillips@rackspace.com>
2013-12-05 09:16:22 -0600

    refactor main

--- bin/agent.js
+++ bin/agent.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var Stream = require('../lib/stream').Stream;
 var winston = require('winston');
 var fs = require('fs');
@@ -12,33 +14,34 @@ var logger = new (winston.Logger)({
 
 logger.setLevels(winston.config.syslog.levels);
 
-// Parse Credentials
+function main() {
+  // Parse Credentials
+  var creds = fs.readFileSync('creds.json');
+  try {
+    creds = JSON.parse(creds);
+  } catch (e) {
+    logger.error('Credential file failed to parse : ' + e.message);
+    process.exit(1);
+  }
+  // Setup Options
 
-var creds = fs.readFileSync('creds.json');
+  var options = {
+    endpoints: [
+      '_monitoringagent._tcp.dfw1.prod.monitoring.api.rackspacecloud.com',
+      '_monitoringagent._tcp.ord1.prod.monitoring.api.rackspacecloud.com',
+      '_monitoringagent._tcp.lon3.prod.monitoring.api.rackspacecloud.com'
+    ],
+    log: logger,
+    ca: [ fs.readFileSync('maas.cert') ],
+    token: creds.token,
+    agent_id: creds.agent_id,
+    guid: '986783af-22ba-4fd0-8615-7b326c61153d',
+    process_version: '0.1.2',
+    bundle_version: '0.1.2'
+  };
 
-try {
-  creds = JSON.parse(creds);
-} catch (e) {
-  logger.error('Credential file failed to parse : ' + e.message);
-  process.exit(1);
+  s = new Stream(options);
+  s.connect();
 }
 
-// Setup Options
-
-var options = {
-  endpoints: [
-    '_monitoringagent._tcp.dfw1.prod.monitoring.api.rackspacecloud.com',
-    '_monitoringagent._tcp.ord1.prod.monitoring.api.rackspacecloud.com',
-    '_monitoringagent._tcp.lon3.prod.monitoring.api.rackspacecloud.com'
-  ],
-  log: logger,
-  ca: [ fs.readFileSync('maas.cert') ],
-  token: creds.token,
-  agent_id: creds.agent_id,
-  guid: '986783af-22ba-4fd0-8615-7b326c61153d',
-  process_version: '0.1.2',
-  bundle_version: '0.1.2'
-};
-
-s = new Stream(options);
-s.connect();
+main();
