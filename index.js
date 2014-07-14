d9256ef496551f5bf8f5b8acc46916cc70e046cb
Song Gao <song@gao.io>
2014-07-14 12:17:56 +0800

    use logmagic; fix some bugs introduced in previous commits

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var logmagic = require('logmagic');
+
+logmagic.route("virgojs.*", logmagic.DEBUG, "graylog2-stderr")
+
 module.exports = {
   'agent': require('./lib/agent'),
   'endpoint': require('./lib/endpoint'),
