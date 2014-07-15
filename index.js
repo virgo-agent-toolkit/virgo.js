5885eb7d7476fc78298a01e0cb1a43128ec196e8
Song Gao <song@gao.io>
2014-07-15 20:52:24 +0800

    namings

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ logmagic.route("virgojs.*", logmagic.DEBUG, "graylog2-stderr");
 module.exports = {
   'agent': require('./lib/agent'),
   'endpoint': require('./lib/endpoint'),
-  'connection': require('./lib/connection'),
+  'Connection': require('./lib/Connection'),
   'heartbeats': require('./lib/heartbeats')
 };
